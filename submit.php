9ae123acd1d51b9bc4a89d2296bb8844544ab79d
navinkrv <97692286+navinkrv@users.noreply.github.com>
2022-08-15 17:32:57 +0530

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,20 @@
+<?php
+$option1= $_POST['service'];
+$option2= $_POST['doctor'];
+$option3= $_POST['time'];
+$name= $_POST['name'];
+$phone= $_POST['phone'];
+
+
+
+echo $option1;
+echo "<br>";
+echo $option2;
+echo "<br>";
+echo $option3;
+echo "<br>";
+echo $name;
+echo "<br>";
+echo $phone;
+
+?>
